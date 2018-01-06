3fd2dde8fe1f03b1a404201c414c4e2db0d29097
ariefati <ariefati@gmail.com>
2018-01-06 13:37:46 +0700

    Update multi-link.js

--- multi-link.js
+++ multi-link.js
@@ -27,7 +27,7 @@ if(x=="5"){xxx="/2018/01/iphone-8-price-and-specification-2018.html"}
 if(x=="6"){xxx="/2018/01/iphone-7-price-and-specification-in.html"}
 if(x=="7"){xxx="/2018/01/best-smartphone-for-photography.html"}
 if(x=="8"){xxx="/2018/01/best-smartphone-for-vlogging.html"}
-if(x=="9"){xxx="/2017/12/data8.html"}
+if(x=="9"){xxx="/2018/01/best-smartphone-for-music.html"}
 if(x=="10"){xxx="/2017/12/data9.html"}a_to_ve[i].href="http://cloud.fw27.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
