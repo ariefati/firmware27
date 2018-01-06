fd56f2c5fe272fad2ca8641f8ac10903373a17de
ariefati <ariefati@gmail.com>
2018-01-06 11:50:07 +0700

    Update multi-link.js

--- multi-link.js
+++ multi-link.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*10)+ 1);var xxx=null;
-if(x=="1"){xxx="/2017/12/data0.html"}
+if(x=="1"){xxx="/2018/01/the-dazling-samsung-galaxy-s6-edge.html"}
 if(x=="2"){xxx="/2017/12/data1.html"}
 if(x=="3"){xxx="/2017/12/data2.html"}
 if(x=="4"){xxx="/2017/12/data3.html"}
